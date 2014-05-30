983217cf3868f2d6c19fc08f5213dc4f5276611c
domiii <dominik.seifert@gmail.com>
2014-05-31 00:49:55 +0800

    - updated squishy

--- node_modules/squishy/index.js
+++ node_modules/squishy/index.js
@@ -5,7 +5,3 @@
 "use strict";
 
 module.exports = require('./lib/squishy');
-
-// also attach Stacktrace and CodeBuilder to squishy for now (until we find a better place for them)
-squishy.Stacktrace = require('./lib/squishy.Stacktrace');
-squishy.CodeBuilder = require('./lib/squishy.CodeBuilder');
