f40d0b4857d417c68fc93b81b14a608d999a87f5
domiii <dominik.seifert@gmail.com>
2014-05-30 19:25:04 +0800

    - initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * This file provides the public interface for accessing components functionality.
+ */
+/*jslint node: true */
+"use strict";
+
+module.exports = {
+	/** 
+	 * ComponentDef gives all the registration methods and access to Shared component object on host.
+	 */
+	Def: require('./lib/ComponentDef'),
+
+	/**
+	 * ComponentLoader provides the `start` method to install & bootstrap the whole bunch.
+	 */
+	Loader: require('./lib/ComponentLoader'),
+
+	/**
+	 * ComponentBootstrap is useful for the advanced user:
+	 * You can provide your own bootstrap implementation to bootstrap components to things other than the default browser environment (e.g. webworker).
+	 */
+	Bootstrap: require('./lib/ComponentBootstrap'),
+
+	/**
+	 * ComponentCommunications is also useful for the advanced user:
+	 * You can provide your own connection implementation to use other ways of transporting requests between host + client (e.g. websockets).
+	 */
+	Communications: require('./lib/ComponentBootstrap')
+};
